ec4067f33fab6590aa329403422559ef6f0f3c44
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-06-18 12:52:02 +0530

    Create lecture1.java

---
+++ Controlflowst/lecture1.java
@@ -0,0 +1,25 @@
+package Controlflowst;
+class ABC{
+    void display(){
+        System.out.println("hello world");
+    }
+}
+class XYZ  extends ABC{
+    void display(){
+        System.out.println("hello java");
+    }
+    void info(){
+        System.out.println("Anything you want");
+    }
+
+}
+public class lecture1 {
+    public static void main(String[] args) {
+        
+        ABC a = new XYZ();
+        a.display();
+      
+        .display();
+    }
+    
+}
