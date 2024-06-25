35c0d5e7da92f43478377984d7806dd654bbeedb
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-06-25 12:38:50 +0530

    array list

---
+++ Arraylist/Add.java
@@ -0,0 +1,19 @@
+
+package Arraylist;
+import java.util.*;
+
+import java.util.ArrayList;
+
+public class Add {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list);
+        System.out.println(list.remove(3));
+        list.Contains(4);
+    }
+}
