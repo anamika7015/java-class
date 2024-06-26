a187b5d1dde3db61d41d99f514c65674fe35d292
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-06-26 10:28:46 +0530

    update

--- Arraylist/Linklist.java
+++ Arraylist/Linklist.java
@@ -1,13 +1,13 @@
 package Arraylist;
-import java.util.LinkedList;
+import java.util.HashSet;
 public class Linklist {
     public static void main(String[] args) {
-        LinkedList<String> names = new LinkedList<>();
-        names.addFirst("table");
+        HashSet<String> names = new HashSet<>();
+        // names.addFirst("table");
         names.add("car");
         names.add("bike");
         names.add("phone");
-        names.addLast("bycycle");
+        // names.addLast("bycycle");
         System.out.println(names);
         names.remove("phone");
         for (String x: names){
