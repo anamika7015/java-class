02217045f904c10ff1d6a9d0614d3758d84e2d02
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-06-27 10:58:21 +0530

    map

---
+++ Controlflowst/Map.java
@@ -0,0 +1,15 @@
+package Controlflowst;
+import java.util.HashMap;
+public class Map {
+    public static void main(String[] args) {
+        HashMap<String,Integer> marks = new HashMap<>();
+        marks.put("priya",53);
+        marks.put("neha",72);
+        marks.put("shweta",58);
+        marks.put("anamika",75);
+        marks.put("putul", 67);
+        System.out.println(marks);
+
+
+    }
+}
